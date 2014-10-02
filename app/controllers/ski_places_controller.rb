a294986d57d8a565495e4e6336ba05d4e8413c69
Andy Mention <amention@gmail.com>
2014-10-02 00:51:28 -0600

    fix leaflet css, add ski_place info to gon variable for show page, change map sizes between index and show pages

--- app/controllers/ski_places_controller.rb
+++ app/controllers/ski_places_controller.rb
@@ -5,5 +5,10 @@ class SkiPlacesController < ApplicationController
 
   def show
     @ski_place = SkiPlace.find(params[:id])
+
+    factory = RGeo::GeoJSON::EntityFactory.instance
+
+    gon.ski_place = RGeo::GeoJSON.encode(factory.feature(@ski_place.geometry, nil, {name: "#{@ski_place.name}", id: "#{@ski_place.id}"}))
+
   end
 end
