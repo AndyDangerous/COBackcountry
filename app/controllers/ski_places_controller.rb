6395f9fca3d6fecf16622240ea4a7eaf2070fda3
Andy Mention <amention@gmail.com>
2014-10-08 15:20:02 -0600

    JSON won't parse and I don't know why

--- app/controllers/ski_places_controller.rb
+++ app/controllers/ski_places_controller.rb
@@ -1,4 +1,9 @@
+require 'geo_ruby/gpx4r/gpx'
+require 'geo_ruby/geojson'
+
 class SkiPlacesController < ApplicationController
+
+
   def new
     @ski_place = SkiPlace.new
   end
@@ -20,17 +25,19 @@ class SkiPlacesController < ApplicationController
     gon.ski_place = RGeo::GeoJSON.encode(factory.feature(@ski_place.geometry, nil, {name: "#{@ski_place.name}", id: "#{@ski_place.id}"}))
   end
 
-  private
-
   def ski_place_params
 
     safe_params = params.require(:ski_place).permit(:name, :description, :geometry)
     update_params(safe_params)
   end
 
+
   def update_params(columns)
-    thing = columns[:geometry].read
-    feature = RGeo::GeoJSON.decode(thing, json_parser: :json)
+    ski_geom = GpxParser.parse(columns[:geometry])
+    binding.pry
+
+    feature = RGeo::GeoJSON.decode(ski_geom, json_parser: :json)
+
     if feature.respond_to?(:geometry)
       columns[:geometry] = feature.geometry
     else
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ feature_collection = RGeo::GeoJSON.decode(avy_zones, json_parser: :json)
 feature_collection.each do |feature|
   # features = RGeo::GeoJSON.decode(zone, json_parser: :json)
   AvalancheForecastZone.set_rgeo_factory_for_column(:the_geom,
-   RGeo::Geographic.spherical_factory(srid: 4326))
+  RGeo::Geographic.spherical_factory(srid: 4326))
   AvalancheForecastZone.create(the_geom: feature.geometry,
                               name: feature['name'],
                               zone_url: feature['zone_url'],
