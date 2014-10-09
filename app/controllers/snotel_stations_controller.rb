c3da109dac403c77b175309da53655c3a21e88ce
Andy Mention <amention@gmail.com>
2014-10-08 20:10:23 -0600

    remove old georuby requires

--- app/controllers/snotel_stations_controller.rb
+++ app/controllers/snotel_stations_controller.rb
@@ -2,12 +2,28 @@ class SnotelStationsController < ApplicationController
   respond_to :json
 
   def index
+    factory = RGeo::GeoJSON::EntityFactory.instance
     @stations = SnotelStation.all
+
+    @stations.each do |station|
+    @station = RGeo::GeoJSON.encode(factory.feature(@station.the_geom, nil,
+                        {name:      @station.name,
+                         timezone:  @station.timezone,
+                         elevation: @station.elevation,
+                         wind:      @station.wind,
+                         triplet:   @station.triplet}))
     respond_with @stations
   end
 
   def show
-    @station = SnotelStation.fine(params[:id])
+    factory = RGeo::GeoJSON::EntityFactory.instance
+    @station = SnotelStation.find(params[:id])
+    @station = RGeo::GeoJSON.encode(factory.feature(@station.the_geom, nil,
+                        {name:      @station.name,
+                         timezone:  @station.timezone,
+                         elevation: @station.elevation,
+                         wind:      @station.wind,
+                         triplet:   @station.triplet}))
     respond_with @station
   end
 end
