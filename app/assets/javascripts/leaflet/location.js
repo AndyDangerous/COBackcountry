30dcb91a5fb50ebf5ae305ae2fd0f00906567af9
Andy Mention <amention@gmail.com>
2014-10-10 14:49:05 -0600

    Add to map is undefined

--- app/assets/javascripts/leaflet/location.js
+++ app/assets/javascripts/leaflet/location.js
@@ -8,7 +8,7 @@
 //   // L.tileLayer('http://{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
 //   //   attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
 //   //   subdomains: ['otile1', 'otile2', 'otile3', 'otile4'],
-//   //   maxZoom: 18
+//   //   maxZoom: 14
 //   // }).addTo(map);
 //   fourteeners = L.geoJson().addTo(map);
 //   _ref = gon.mountains;
--- app/assets/javascripts/leaflet/one_geojson_to_leaflet.js
+++ app/assets/javascripts/leaflet/one_geojson_to_leaflet.js
@@ -8,11 +8,26 @@ $(document).on("ready page:load", function(){
   L.tileLayer('http://{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
     subdomains: ['otile1', 'otile2', 'otile3', 'otile4'],
-    maxZoom: 18
+    maxZoom: 14
   }).addTo(map);
 
+  debugger
+
   geojsonUrl = "snotel_stations/2.json";
-  geojsonLayer = L.GeoJSON().addTo(map);
+  geojsonLayer = L.geoJson.addTo(map);
   geojsonLayer.addData(geojsonUrl);
 
 })
+
+
+// $.getJSON( "snotel_stations/2.json", function( data ) {
+//   var items = [];
+//   $.each( data, function( key, val ) {
+//     items.push( "<li id='" + key + "'>" + val + "</li>" );
+//   });
+//
+//   $( "<ul/>", {
+//     "class": "my-new-list",
+//     html: items.join( "" )
+//   }).appendTo( "body" );
+// });
