3158f8eaa4fdd9d466e68ef51de95ab25b0066ed
Andy Mention <amention@gmail.com>
2014-10-14 15:39:59 -0600

    Fix find_centroid, find_avalanche_forecast_zone, and find_snotel in SkiPlaceGeo

--- app/models/ski_place_geo.rb
+++ app/models/ski_place_geo.rb
@@ -1,6 +1,6 @@
 class SkiPlaceGeo
   def self.find_centroid(ski_place)
-    SkiPlace.set_rgeo_factory_for_column(:centroid, RGeo::Geographic.spherical_factory(srid: 4326))
+    SkiPlace.set_rgeo_factory_for_column(:centroid, RGeo::Geos.factory(srid: 4326))
         geometry = ski_place.geometry
 
         query = "SELECT ST_AsText(ST_Centroid('#{geometry.as_text}'))"
@@ -15,18 +15,20 @@ class SkiPlaceGeo
     zones = AvalancheForecastZone.all
 
     facts = zones.map do |zone|
-      zone.the_geom.intersects?(centroid)
+      # zone.the_geom.intersects?(centroid)
+      centroid.within?(zone.the_geom)
     end
+    binding.pry
     ski_place.avalanche_forecast_zone_id = facts.find_index(true) + 1
     ski_place.save
   end
 
   def self.find_snotel(ski_place)
-    p = ski_place.centroid
+    centroid = ski_place.centroid
     snotels = SnotelStation.all
 
     ski_place.snotel_station_id = snotels.sort_by do |station|
-       station.the_geom.distance(p.geometry)
+       station.the_geom.distance(centroid)
     end.first.id
     ski_place.save
   end
