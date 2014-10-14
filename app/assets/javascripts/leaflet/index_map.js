95702a265c91bd971b3d3fe938de82e80a886172
Andy Mention <amention@gmail.com>
2014-10-14 15:05:13 -0600

    This isn't how I'm going to do this

--- app/assets/javascripts/leaflet/index_map.js
+++ app/assets/javascripts/leaflet/index_map.js
@@ -8,33 +8,36 @@ $(document).on("ready page:load", function(){
 
   L.tileLayer('http://{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
-    subdomains: ['otile1', 'otile2', 'otile3', 'otile4'],
     maxZoom: 14
   }).addTo(map);
-
   mahLayer = L.layerGroup();
 
-var numberOfGeometries = gon.numberOfGeometries || 1;
-for (var i = 1; i <= numberOfGeometries; i++) {
-    var counter = gon.counter || i;
+// var numberOfGeometries = gon.numberOfGeometries || 1;
+// for (var i = 1; i <= numberOfGeometries; i++) {
+    // var counter = gon.counter || i;
 
-    geojsonUrl = "/api/v1/"+ gon.geometry_url +"/"+ counter +".json";
+    geojsonUrl = "/api/v1/"+ gon.geometry_url +".json";
 
       $.getJSON(geojsonUrl, function(data){
         console.log("success")
-          L.geoJson(data, {
-            style: function (feature) {
-                return {color: "red", radius: 5};
-            },
 
-            onEachFeature: function (feature, layer) {
-                layer.bindPopup(
-    					  '<a href="/ski_places/'+ feature.properties.id +'"> '+ feature.properties.name +'</a>'
-    					  );
-            }
-          }).addTo(mahLayer);
+          for(var i = 0; i < data.length; i++) {
+            L.geoJson(data, {
+              style: function (feature) {
+                  return {color: "red", radius: 5};
+              },
+
+              onEachFeature: function (feature, layer) {
+                  layer.bindPopup(
+      					  '<a href="/ski_places/'+ feature.properties.id +'"> '+ feature.properties.name +'</a>'
+      					  );
+              }
+            }).addTo(mahLayer);
+
+          }
+
       });
-}
-mahLayer.addTo(map);
+// }
+      mahLayer.addTo(map);
 
-})
+});
